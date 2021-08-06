1168367b25bebfaaa84f121c3c5af1c47aa06d74
rbpolim <rodrigobpolim@gmail.com>
2021-08-06 09:05:52 -0300

    10.5-stripe-notion-parte3-done

---
+++ src/pages/api/webhook.js
@@ -0,0 +1,64 @@
+import { buffer } from "micro";
+import * as admin from "firebase-admin";
+
+const serviceAccount = require("../../../permissions.json");
+const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+  : admin.app();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+const fulfillOrder = async (session) => {
+  return app
+    .firestore()
+    .collection("users")
+    .doc(session.metadata.email)
+    .collection("orders")
+    .doc(session.id)
+    .set({
+      amount: session.amount_total / 100,
+      amount_shipping: session.total_details.amount_shipping / 100,
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+      console.log(`Success: Order ${session.id} had been added to the DB.`);
+    });
+};
+
+export default async (request, response) => {
+  if (request.method === "POST") {
+    const requestBuffer = await buffer(request);
+    const payload = requestBuffer.toString();
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (err) {
+      console.log("Error", err.message);
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      return fulfillOrder(session)
+        .then(() => response.status(200))
+        .catch((err) =>
+          response.status(400).send(`Webhook Error: ${err.message}`)
+        );
+    }
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
